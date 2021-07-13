3680bb3d433d782fa7e3572e5803138cf494cdfe
hey-chan <liumatthew8@gmail.com>
2021-07-13 22:51:23 +1000

    Added dependent destroy for listing models

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,7 @@
 module ListingsHelper
+  def image_select(listing)
+    return listing.picture if listing.picture.attached?
+      # if no image, will default to below
+    return "coathanger.jpg"
+  end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :listings_features
+  has_many :listings_features, dependent: :destroy
   has_many :features, through: :listings_features
   accepts_nested_attributes_for :listings_features
   enum condition: {damaged: 0, poor: 1, good: 2, excellent: 3, mint: 4}
